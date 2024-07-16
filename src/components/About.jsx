abdba620b7d03d63c63cb29554a87d9e2020c9f8
Dang Hoang Ha <ha.dh290702@gmail.com>
2024-07-16 21:08:59 +0300

    fix bug

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import { motion } from 'framer-motion';
 import React from 'react';
 import { Tilt } from 'react-tilt';
 import { styles } from '../styles';
-import { services } from '../contants';
+import { services } from '../constants';
 import { fadeIn, textVariant } from '../utils/motion';
 import { SectionWrapper } from '../hoc';
 const ServiceCard = ({ index, title, icon }) => {
